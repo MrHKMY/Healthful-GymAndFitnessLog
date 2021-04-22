5d6689ea57d93dea344e3769943acfffc0d78eec
MrHKMY <mrhakimi88@gmail.com>
2021-04-22 14:35:00 +0800

    Update Profile UI 2.0

--- lib/screens.dart
+++ lib/screens.dart
@@ -78,6 +78,12 @@ class _CalendarScreenState extends State<CalendarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("App Name"),
+        elevation: 10,
+        centerTitle: true,
+        backgroundColor: Color(0xff374250),
+      ),
       backgroundColor: Color(0xff465466),
       body: Stack(children: [
         Container(
@@ -338,99 +344,208 @@ class _ProgressScreenState extends State<ProgressScreen> {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
 
-    return SafeArea(
-        child: Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Progress Tracker"),
+        elevation: 10,
+        centerTitle: true,
+        backgroundColor: Color(0xff374250),
+      ),
       resizeToAvoidBottomInset: false,
       backgroundColor: Color(0xff465466),
-      body: Stack(children: [
-        Container(
-          margin: EdgeInsets.all(50),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/muscle_transparent.png"),
-                fit: BoxFit.cover),
-          ),
+      body: SafeArea(
+        child: Stack(children: [
+    Container(
+        margin: EdgeInsets.all(50),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/muscle_transparent.png"),
+              fit: BoxFit.cover),
         ),
-        Positioned(
-            top: height * 0.01,
-            right: width * 0.35,
-            left: width * 0.35,
-            child: GestureDetector(
+    ),
+    Positioned(
+          top: height * 0.01,
+          right: width * 0.35,
+          left: width * 0.35,
+          child: GestureDetector(
+            onTap: () {
+              _showSingleDialog("Weight");
+            },
+            child: FutureBuilder(
+                future: _dbHelper.retrieve1Part("Weight"),
+                builder: (context, snapshot) {
+                  return WeightWidget(
+                      parts: "Weight",
+                      //activity: snapshot.data[index].activity,
+                      //date: snapshot.data[index].date,
+                      measurement: snapshot.data.toString() != "null"
+                          ? snapshot.data.toString()
+                          : "Tap to input");
+                }),
+          )),
+    Positioned(
+          top: height * 0.2,
+          left: width * 0.09,
+          child: GestureDetector(
+            onTap: () {
+              _showSingleDialog("Chest");
+            },
+            child: FutureBuilder(
+                future: _dbHelper.retrieve1Part("Chest"),
+                builder: (context, snapshot) {
+                  return WeightWidget(
+                      parts: "Chest",
+                      measurement: snapshot.data.toString() != "null"
+                          ? snapshot.data.toString()
+                          : "Tap to input");
+                }),
+          )),
+    Positioned(
+          top: height * 0.3,
+          left: width * 0.09,
+          child: GestureDetector(
+            onTap: () {
+              _showSingleDialog("Waist");
+            },
+            child: FutureBuilder(
+                future: _dbHelper.retrieve1Part("Waist"),
+                builder: (context, snapshot) {
+                  return WeightWidget(
+                      parts: "Waist",
+                      measurement: snapshot.data.toString() != "null"
+                          ? snapshot.data.toString()
+                          : "Tap to input");
+                }),
+          )),
+    Positioned(
+          top: height * 0.4,
+          left: width * 0.09,
+          child: GestureDetector(
+            onTap: () {
+              _showSingleDialog("Hips");
+            },
+            child: FutureBuilder(
+                future: _dbHelper.retrieve1Part("Hips"),
+                builder: (context, snapshot) {
+                  return WeightWidget(
+                      parts: "Hips",
+                      measurement: snapshot.data.toString() != "null"
+                          ? snapshot.data.toString()
+                          : "Tap to input");
+                }),
+          )),
+    Positioned(
+          top: height * 0.2,
+          right: width * 0.07,
+          child: GestureDetector(
+            onTap: () {
+              _showDoubleDialog("Upper Arm");
+            },
+            child: FutureBuilder(
+                future: _dbHelper.retrieve2Part("Upper Arm"),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    // Uncompleted State
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator());
+                      break;
+                    default:
+                      // Completed with error
+                      if (snapshot.hasError)
+                        return Container(
+                            child: Text(snapshot.error.toString()));
+                      return ArmWidget(
+                          twoPart: "Upper Arm",
+                          leftMeasurement:
+                              snapshot.data[0].toString() != "null"
+                                  ? snapshot.data[0].toString()
+                                  : "Tap to input",
+                          rightMeasurement:
+                              snapshot.data[1].toString() != "null"
+                                  ? snapshot.data[1].toString()
+                                  : "Tap to input");
+                  }
+                }),
+          )),
+    Positioned(
+          top: height * 0.35,
+          right: width * 0.07,
+          child: GestureDetector(
+            onTap: () {
+              _showDoubleDialog("Forearm");
+            },
+            child: FutureBuilder(
+                future: _dbHelper.retrieve2Part("Forearm"),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    // Uncompleted State
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator());
+                      break;
+                    default:
+                      // Completed with error
+                      if (snapshot.hasError)
+                        return Container(
+                            child: Text(snapshot.error.toString()));
+                      return ArmWidget(
+                          twoPart: "Forearm",
+                          leftMeasurement:
+                              snapshot.data[0].toString() != "null"
+                                  ? snapshot.data[0].toString()
+                                  : "Tap to input",
+                          rightMeasurement:
+                              snapshot.data[1].toString() != "null"
+                                  ? snapshot.data[1].toString()
+                                  : "Tap to input");
+                  }
+                }),
+          )),
+    Positioned(
+          top: height * 0.53,
+          left: width * 0.09,
+          child: GestureDetector(
+            onTap: () {
+              _showDoubleDialog("Thigh");
+            },
+            child: FutureBuilder(
+                future: _dbHelper.retrieve2Part("Thigh"),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    // Uncompleted State
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator());
+                      break;
+                    default:
+                      // Completed with error
+                      if (snapshot.hasError)
+                        return Container(
+                            child: Text(snapshot.error.toString()));
+                      return ArmWidget(
+                          twoPart: "Thigh",
+                          leftMeasurement:
+                              snapshot.data[0].toString() != "null"
+                                  ? snapshot.data[0].toString()
+                                  : "Tap to input",
+                          rightMeasurement:
+                              snapshot.data[1].toString() != "null"
+                                  ? snapshot.data[1].toString()
+                                  : "Tap to input");
+                  }
+                }),
+          )),
+    Positioned(
+          top: height * 0.68,
+          left: width * 0.09,
+          child: GestureDetector(
               onTap: () {
-                _showSingleDialog("Weight");
+                _showDoubleDialog("Calf");
               },
               child: FutureBuilder(
-                  future: _dbHelper.retrieve1Part("Weight"),
-                  builder: (context, snapshot) {
-                    return WeightWidget(
-                        parts: "Weight",
-                        //activity: snapshot.data[index].activity,
-                        //date: snapshot.data[index].date,
-                        measurement: snapshot.data.toString() != "null"
-                            ? snapshot.data.toString()
-                            : "Tap to input");
-                  }),
-            )),
-        Positioned(
-            top: height * 0.2,
-            left: width * 0.09,
-            child: GestureDetector(
-              onTap: () {
-                _showSingleDialog("Chest");
-              },
-              child: FutureBuilder(
-                  future: _dbHelper.retrieve1Part("Chest"),
-                  builder: (context, snapshot) {
-                    return WeightWidget(
-                        parts: "Chest",
-                        measurement: snapshot.data.toString() != "null"
-                            ? snapshot.data.toString()
-                            : "Tap to input");
-                  }),
-            )),
-        Positioned(
-            top: height * 0.3,
-            left: width * 0.09,
-            child: GestureDetector(
-              onTap: () {
-                _showSingleDialog("Waist");
-              },
-              child: FutureBuilder(
-                  future: _dbHelper.retrieve1Part("Waist"),
-                  builder: (context, snapshot) {
-                    return WeightWidget(
-                        parts: "Waist",
-                        measurement: snapshot.data.toString() != "null"
-                            ? snapshot.data.toString()
-                            : "Tap to input");
-                  }),
-            )),
-        Positioned(
-            top: height * 0.4,
-            left: width * 0.09,
-            child: GestureDetector(
-              onTap: () {
-                _showSingleDialog("Hips");
-              },
-              child: FutureBuilder(
-                  future: _dbHelper.retrieve1Part("Hips"),
-                  builder: (context, snapshot) {
-                    return WeightWidget(
-                        parts: "Hips",
-                        measurement: snapshot.data.toString() != "null"
-                            ? snapshot.data.toString()
-                            : "Tap to input");
-                  }),
-            )),
-        Positioned(
-            top: height * 0.2,
-            right: width * 0.07,
-            child: GestureDetector(
-              onTap: () {
-                _showDoubleDialog("Upper Arm");
-              },
-              child: FutureBuilder(
-                  future: _dbHelper.retrieve2Part("Upper Arm"),
+                  //initialData: [],
+                  future: _dbHelper.retrieve2Part("Calf"),
                   builder: (context, snapshot) {
                     switch (snapshot.connectionState) {
                       // Uncompleted State
@@ -444,133 +559,31 @@ class _ProgressScreenState extends State<ProgressScreen> {
                           return Container(
                               child: Text(snapshot.error.toString()));
                         return ArmWidget(
-                            twoPart: "Upper Arm",
+                            twoPart: "Calf",
                             leftMeasurement:
                                 snapshot.data[0].toString() != "null"
-                                    ? snapshot.data[0].toString()
+                                    ? snapshot.data[0]
                                     : "Tap to input",
                             rightMeasurement:
                                 snapshot.data[1].toString() != "null"
-                                    ? snapshot.data[1].toString()
+                                    ? snapshot.data[1]
                                     : "Tap to input");
                     }
-                  }),
-            )),
-        Positioned(
-            top: height * 0.35,
-            right: width * 0.07,
-            child: GestureDetector(
-              onTap: () {
-                _showDoubleDialog("Forearm");
-              },
-              child: FutureBuilder(
-                  future: _dbHelper.retrieve2Part("Forearm"),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      // Uncompleted State
-                      case ConnectionState.none:
-                      case ConnectionState.waiting:
-                        return Center(child: CircularProgressIndicator());
-                        break;
-                      default:
-                        // Completed with error
-                        if (snapshot.hasError)
-                          return Container(
-                              child: Text(snapshot.error.toString()));
-                        return ArmWidget(
-                            twoPart: "Forearm",
-                            leftMeasurement:
-                                snapshot.data[0].toString() != "null"
-                                    ? snapshot.data[0].toString()
-                                    : "Tap to input",
-                            rightMeasurement:
-                                snapshot.data[1].toString() != "null"
-                                    ? snapshot.data[1].toString()
-                                    : "Tap to input");
-                    }
-                  }),
-            )),
-        Positioned(
-            top: height * 0.53,
-            left: width * 0.09,
-            child: GestureDetector(
-              onTap: () {
-                _showDoubleDialog("Thigh");
-              },
-              child: FutureBuilder(
-                  future: _dbHelper.retrieve2Part("Thigh"),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      // Uncompleted State
-                      case ConnectionState.none:
-                      case ConnectionState.waiting:
-                        return Center(child: CircularProgressIndicator());
-                        break;
-                      default:
-                        // Completed with error
-                        if (snapshot.hasError)
-                          return Container(
-                              child: Text(snapshot.error.toString()));
-                        return ArmWidget(
-                            twoPart: "Thigh",
-                            leftMeasurement:
-                                snapshot.data[0].toString() != "null"
-                                    ? snapshot.data[0].toString()
-                                    : "Tap to input",
-                            rightMeasurement:
-                                snapshot.data[1].toString() != "null"
-                                    ? snapshot.data[1].toString()
-                                    : "Tap to input");
-                    }
-                  }),
-            )),
-        Positioned(
-            top: height * 0.68,
-            left: width * 0.09,
-            child: GestureDetector(
-                onTap: () {
-                  _showDoubleDialog("Calf");
-                },
-                child: FutureBuilder(
-                    //initialData: [],
-                    future: _dbHelper.retrieve2Part("Calf"),
-                    builder: (context, snapshot) {
-                      switch (snapshot.connectionState) {
-                        // Uncompleted State
-                        case ConnectionState.none:
-                        case ConnectionState.waiting:
-                          return Center(child: CircularProgressIndicator());
-                          break;
-                        default:
-                          // Completed with error
-                          if (snapshot.hasError)
-                            return Container(
-                                child: Text(snapshot.error.toString()));
-                          return ArmWidget(
-                              twoPart: "Calf",
-                              leftMeasurement:
-                                  snapshot.data[0].toString() != "null"
-                                      ? snapshot.data[0]
-                                      : "Tap to input",
-                              rightMeasurement:
-                                  snapshot.data[1].toString() != "null"
-                                      ? snapshot.data[1]
-                                      : "Tap to input");
-                      }
-                    }))),
-        Positioned(
-          right: width * 0.1,
-          bottom: MediaQuery.of(context).viewInsets.bottom > 0
-              ? 0.0
-              : kBottomNavigationBarHeight + 50,
-          child: FloatingActionButton(
-            //todo(3) create fab to open new charts screen
-            onPressed: () {},
-            child: Icon(Icons.bar_chart_rounded),
-          ),
-        )
-      ]),
-    ));
+                  }))),
+    Positioned(
+        right: width * 0.1,
+        bottom: MediaQuery.of(context).viewInsets.bottom > 0
+            ? 0.0
+            : kBottomNavigationBarHeight + 50,
+        child: FloatingActionButton(
+          //todo(3) create fab to open new charts screen
+          onPressed: () {},
+          child: Icon(Icons.bar_chart_rounded),
+        ),
+    )
+        ]),
+      ),
+    );
   }
 
   _showSingleDialog(String part) async {
