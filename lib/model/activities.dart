6c89502dbe684da179097580344faa5b974843f9
MrHKMY <mrhakimi88@gmail.com>
2021-05-09 23:50:48 +0800

    Update Calendar Dialog (3)

--- lib/model/activities.dart
+++ lib/model/activities.dart
@@ -1,13 +1,17 @@
 class Activities {
   final int id;
   final String activity;
+  final String focus;
+  final int setCount;
   final String date;
-  Activities({this.id, this.activity, this.date});
+  Activities({this.id, this.activity, this.focus, this.setCount, this.date});
 
   Map<String, dynamic> toMap() {
     return{
       "id" : id,
       "Activity" : activity,
+      "Focus" : focus,
+      "SetCount" : setCount,
       "Date" : date
     };
   }
